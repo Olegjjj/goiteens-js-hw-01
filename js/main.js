2e3f51fe812577684402716027024f28492a1555
Oleg Plaksin <plaksinoleg53@gmail.com>
2023-05-31 21:45:52 +0300

    git

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const name = "Oleg";
+console.log(name);
+
+const age = 12;
+console.log(age);
+
+const isStudent = true;
+console.log(isStudent);
+
+const myString = "Початок- є більш ніж половина всього";
+console.log(myString)
+
+myNumber = 1e500 + 10;
+console.log(myNumber)
+
+let myNull = null;
+console.log(myNull);
+
+const isName = prompt("Будь ласка введіть своє ім'я");
+console.log(isName);
+if (isName === isName) alert("Привіт користувач!");
+else alert("Дію відмінено!");
+// console.log(message);
+console.log(isName);
+
+const confirning = confirm("Підтвердити дію?");
+if (confirning === true) alert("Дякую за підтвердження!");
+else alert("Дію відмінено!");
+console.log(confirning);
+
+const dangerous = alert("Дія небезпечна")
+const assurances = confirm("Підтвердити дію?");
+if (assurances === true) alert("Дякую за підтвердження!");
+else alert("Дію відмінено!");
+console.log(assurances);
+console.log(dangerous);
